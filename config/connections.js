7099cd97e4d47aec676b3704c174784bc933717d
mayaj0yce <m.j.thayne@gmail.com>
2023-08-26 22:34:02 -0600

    boil plate connection.js

---
+++ config/connections.js
@@ -0,0 +1,9 @@
+const { connect, connection } = require('mongoose');
+
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
+
+connect(connectionString);
+
+module.exports = connection;
+
